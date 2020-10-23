b8ff5dda8749c41f2f6a105029a6b6e855738708
PratthamArora <arora.prattham@gmail.com>
2020-10-24 00:17:30 +0530

    setup Resource class

--- app/src/main/java/com/pratthamarora/moviedb_clone/ui/trending/TrendingFragment.kt
+++ app/src/main/java/com/pratthamarora/moviedb_clone/ui/trending/TrendingFragment.kt
@@ -2,11 +2,15 @@ package com.pratthamarora.moviedb_clone.ui.trending
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.pratthamarora.moviedb_clone.R
+import com.pratthamarora.moviedb_clone.utils.Status.*
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.layout_loading.*
 import kotlinx.android.synthetic.main.trending_fragment.*
 
 @AndroidEntryPoint
@@ -27,8 +31,22 @@ class TrendingFragment : Fragment(R.layout.trending_fragment) {
 
     private fun subscribeToObservers() {
         viewModel.trendingMovies.observe(viewLifecycleOwner, {
-            it?.let {
-                trendingMoviesAdapter.setMovies(it)
+            it?.let { response ->
+                when (response.status) {
+                    SUCCESS -> {
+                        response.data?.let { it1 -> trendingMoviesAdapter.setMovies(it1) }
+                        showLoading(false)
+                    }
+                    ERROR -> {
+                        showLoading(false)
+                        Snackbar.make(
+                            requireView(),
+                            response.message.toString(),
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                    LOADING -> showLoading(true)
+                }
             }
 
         })
@@ -42,5 +60,8 @@ class TrendingFragment : Fragment(R.layout.trending_fragment) {
         }
     }
 
+    private fun showLoading(isShow: Boolean) {
+        loadingContainer.isVisible = isShow
+    }
 
 }
--- app/src/main/java/com/pratthamarora/moviedb_clone/ui/trending/TrendingViewModel.kt
+++ app/src/main/java/com/pratthamarora/moviedb_clone/ui/trending/TrendingViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.pratthamarora.moviedb_clone.data.model.Movie
 import com.pratthamarora.moviedb_clone.data.repository.MovieRepository
+import com.pratthamarora.moviedb_clone.utils.Resource
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import timber.log.Timber
 
@@ -13,17 +14,23 @@ class TrendingViewModel @ViewModelInject constructor(
     movieRepository: MovieRepository
 ) : ViewModel() {
 
-    private val _trendingMovies = MutableLiveData<List<Movie>>()
-    val trendingMovies: LiveData<List<Movie>> get() = _trendingMovies
+    private val _trendingMovies = MutableLiveData<Resource<List<Movie>>>()
+    val trendingMovies: LiveData<Resource<List<Movie>>> get() = _trendingMovies
 
     init {
         movieRepository.getTrendingMovies()
+            .doOnSubscribe { _trendingMovies.value = Resource.Loading(null) }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { response ->
-                    _trendingMovies.value = response.results
+                    _trendingMovies.value = response.results?.let { Resource.Success(it) }
                 },
-                { error -> Timber.e(error.localizedMessage) }
+                { error ->
+                    run {
+                        Timber.e(error)
+                        _trendingMovies.value = Resource.Error(error.message!!, null)
+                    }
+                }
             )
     }
 }
